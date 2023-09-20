f8962e759a992f048b3e7d79f0c5aac8250c4c89
Mkrtychiyants <heisaloosershesad@gmail.com>
2023-09-20 19:20:01 +1000

    edition2

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 
 const http = require('http');
 const Koa = require('koa')
-const koaBody = require('koa-body')
-const tasksList = require('./tasks.json')
+const koaBody = require('koa-body');
+const tasksList = require('./tasks.json');
+
 const uuid = require('uuid');
 
 const app = new Koa();
@@ -15,91 +16,84 @@ app.use(koaBody({
 
 app.use((ctx, next) => {
   const { method, id } = ctx.request.query
-  if (ctx.request.method !== 'GET') {
-    next();
-    return;
-  }
-  if (method === 'allTickets') {
-    ctx.response.status = 200;
-    ctx.response.set('Access-Control-Allow-Origin', '*');
-    ctx.response.body = JSON.stringify(tasksList);
-    next();
-  }
-  if (method === 'ticketById') {
-
-    ctx.response.status = 200;
-    ctx.response.set('Access-Control-Allow-Origin', '*');
-
-    const currTaskDescr = tasksList.find((el) => el.id == id);
-    if (currTaskDescr) {
-      ctx.response.body = JSON.stringify(currTaskDescr);
-    } else {
-      ctx.response.body = "description not finded";
+  if (ctx.request.method === 'GET') {
+    if (method === 'allTickets') {
+      ctx.response.status = 200;
+      ctx.response.set('Access-Control-Allow-Origin', '*');
+      ctx.response.body = JSON.stringify(tasksList);
+      next();
     }
-
-    next();
-  }
-  if (method === 'updateStatusById') {
-    ctx.response.status = 200;
-    ctx.response.set('Access-Control-Allow-Origin', '*');
-
-    const currTask = tasksList.find((el) => el.id == id);
-
-    if (currTask) {
-      currTask.status = !currTask.status;
-      ctx.response.body = JSON.stringify(currTask.status);
-    } else {
-      ctx.response.body = "task not finded";
+    if (method === 'ticketById') {
+  
+      ctx.response.status = 200;
+      ctx.response.set('Access-Control-Allow-Origin', '*');
+      
+      const currTaskDescr = tasksList.find((element) => element.id===id.trim());
+      if (currTaskDescr) {
+        ctx.response.body = JSON.stringify(currTaskDescr);
+      } else {
+        ctx.response.body = "description not finded";
+      }
+  
+      next();
     }
-
-    next();
-  }
-  if (method === 'deleteById') {
-    ctx.response.status = 200;
-    ctx.response.set('Access-Control-Allow-Origin', '*');
-
-    const currTaskInd = tasksList.indexOf((el) => el.id == id);
-
-    if (currTaskInd) {
-      tasksList.splice(currTaskInd, 1)
-    } else {
-      ctx.response.body = "task not found";
+    if (method === 'updateStatusById') {
+      ctx.response.status = 200;
+      ctx.response.set('Access-Control-Allow-Origin', '*');
+      
+      const currTaskStatus = tasksList.find((element) => element.id===id.trim());
+      if (currTaskStatus) {
+        currTaskStatus.status = !currTaskStatus.status;
+        ctx.response.body = JSON.stringify(currTaskStatus);
+      } else {
+        ctx.response.body = "task not finded";
+      }
+  
+      next();
     }
+    if (method === 'deleteById') {
+      ctx.response.status = 200;
+      ctx.response.set('Access-Control-Allow-Origin', '*');
 
-    next();
+      const currTaskInd = tasksList.findIndex((element) => element.id===id.trim());
+      if (currTaskInd >=0) {
+        tasksList.splice(currTaskInd, 1);
+      } else {
+        ctx.response.body = "task not found";
+      }
+      next();
+    }
   }
+  if (ctx.request.method === 'POST') {
+    const { shortDescr, longDescr } = ctx.request.body;
+    if (method === 'updateById') {
+      const currTaskDescr = tasksList.find((element) => element.id===id.trim());
 
-})
-app.use((ctx, next) => {
-  if (ctx.request.method !== 'POST') {
-    next();
-    return;
+      currTaskDescr.name = shortDescr;
+      currTaskDescr.description = longDescr;
+      ctx.response.status = 200;
+      ctx.response.set('Access-Control-Allow-Origin', '*');
+      ctx.response.body = "OKE";
+      next()
+    }
+    if (method === 'createTicket') {
+      ctx.response.status = 200;
+      ctx.response.set('Access-Control-Allow-Origin', '*');
+      ctx.response.body = "OKE";
+      const date = new Date(Date.now());
+      const newTicket = {
+        id: uuid.v4(),
+        name: shortDescr,
+        status: false,
+        description: longDescr,
+        created: date
+      }
+      tasksList.push(newTicket);
+      next();
+    }
   }
-  const { shortDescr, longDescr } = ctx.request.body;
-  const { method, id } = ctx.request.query
-  if (method === 'updateById') {
-    const currTaskDescr = tasksList.find((el) => el.id == id);
-    currTaskDescr.name = shortDescr;
-    currTaskDescr.description = longDescr;
-    ctx.response.status = 200;
-    ctx.response.set('Access-Control-Allow-Origin', '*');
-    ctx.response.body = "OKE";
-    next()
-  }
-  if (method === 'createTicket') {
-    const date = new Date(Date.now());
-    tasksList.push({
-      id: uuid.v4(),
-      name: shortDescr,
-      status: false,
-      description: longDescr,
-      created: date
-    })
-    ctx.response.status = 200;
-    ctx.response.set('Access-Control-Allow-Origin', '*');
-    ctx.response.body = "OKE";
-    next();
-  }
+  
+
 })
 
 
